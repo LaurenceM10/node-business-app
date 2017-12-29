5f8c8ffef905f947b5c6725d0f9076fb28cb396c
Lauren <laurenmontenegro10@gmail.com>
2017-12-29 10:16:55 -0600

    Added product schema and model with mongoose

--- models/product.js
+++ models/product.js
@@ -2,10 +2,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 //Create a product schema
-let ProductSchema = new Schema({
+let productSchema = new Schema({
     name: String,
     description: String,
     price: Number,
     units: Number,
     picture: Buffer
 });
+
+
+/**
+ * To use our schema definition, we need to convert our productSchema
+ * into a Model we can work with. To do so, we pass it into mongoose.model(modelName, schema)
+ *
+ * Then, we export the model
+ */
+module.exports = mongoose.model('Product', productSchema);
