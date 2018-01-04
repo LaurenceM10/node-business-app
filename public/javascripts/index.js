e8375596ff55f6f9a0d222aa3466bc9f294fbd92
Lauren <laurenmontenegro10@gmail.com>
2018-01-03 19:55:34 -0600

    Added dialog to verify the removal of a product

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,11 +3,12 @@ let iconMenu = document.querySelector('#icon-menu'),
     navigation = document.querySelector('#nav'),
     headerSearch = document.querySelector('#header__search'),
     contentMain = document.querySelector('#content-main'),
-    editIcon = document.querySelector('.icon-edit'),
-    deleteIcon = document.querySelector('.icon-trash'),
+    editIcon = document.querySelectorAll('.icon-edit'),
+    deleteIcon = document.querySelectorAll('.icon-trash'),
     dialog = document.querySelector('#dialog'),
     deleteDialog = document.querySelector('#delete-dialog'),
-    editDialog = document.querySelector('#edit-dialog');
+    editDialog = document.querySelector('#edit-dialog'),
+    deleteDialogButton =  document.querySelector('#dialog-link--delete');
 
 /**
  * Added click a listener to the icon menu and add or delete
@@ -23,31 +24,54 @@ headerSearch.addEventListener('click', (event) => {
 });
 
 contentMain.addEventListener('click', (event) => {
-   navigation.classList.remove('open-menu');
+    navigation.classList.remove('open-menu');
 });
 
 
 /**
- * Show the dialog when a button/icon is clicked
+ * Show the dialog when a button/icon is clicked.
+ * Shoot the event when any item with the same class was clicked.
+ * */
+let showDialog = () => {
+    for (let i = 0; i < deleteIcon.length; i++) {
+        deleteIcon[i].addEventListener('click', (event) => {
+            //Prevent the default action of the 'a' element
+            event.preventDefault();
+
+            //Show the dialog
+            dialog.classList.toggle('dialog--visible');
+
+            //Pass the route with id of the element clicked
+            passLink(deleteIcon[i].getAttribute('href'));
+        });
+    }
+};
+
+//Call the method
+showDialog();
+
+/**
+ * Close the dialog when the cancel button is clicked or when any part outside the dialog is clicked
  *
  * Solution obtained from Stackoverflow in the following link:
  * https://stackoverflow.com/questions/11606897/get-only-first-class-of-an-html-element
- * */
-
-document.querySelector('body').addEventListener('click', (event) => {
-    if (event.target.className.toLowerCase() === 'icon-trash') {
-        dialog.classList.toggle('dialog--visible');
-    }
-});
-
-//Close the dialog when the cancel button is clicked or when any part outside the dialog is clicked
+ */
 document.querySelector('body').addEventListener('click', (event) => {
     if (event.target.id.toLowerCase() === 'dialog-link--cancel' || event.target.id.toLowerCase() === 'dialog') {
         dialog.classList.remove('dialog--visible');
     }
 });
 
-
-
+/*
+* Pass the link to delete for the dialog
+*
+*  A way to pass the route with the link of the specific product
+*  to the dialogue, is to obtain that route from the link of the
+*  garbage icon by clicking it and pass it dynamically to the
+*  'delete' link in the dialog
+* */
+let passLink = (link) => {
+    deleteDialogButton.setAttribute('href', link);
+};
 
 
